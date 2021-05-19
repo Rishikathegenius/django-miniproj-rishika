4cb2fbe7c54bdb6079228be65d88b32a9219f1df
Rishikathegenius <dancefivaella@gmail.com>
2021-05-19 13:22:34 +0530

    holaa

--- dbms_miniproject/retail/models.py
+++ dbms_miniproject/retail/models.py
@@ -5,11 +5,11 @@ class Brands(models.Model):
     brand_name = models.TextField()
     no_of_products=models.IntegerField(default=0)
 
+
 class Products(models.Model):
     product_name = models.TextField()
     product_type = models.TextField()
     product_price = models.IntegerField(default=0)
-    product_image =models.ImageField(upload_to='media')
 
     brand = models.ForeignKey(Brands,on_delete=models.CASCADE, default="")
 class Customer(models.Model):
